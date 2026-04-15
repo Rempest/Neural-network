91d7ada1d4a455ac7144fc6f0748ec8cc1b6c9c3
Rempest <thehuman130410@gmail.com>
2026-04-15 19:36:24 +0500

    Include predictions and plot in training script
    Add predictions and visualization for model output

--- train.py
+++ train.py
@@ -5,5 +5,8 @@ x = np.array([1, 2, 3, 4, 5], dtype = float)
 y = np.array([2, 4, 6, 8, 10], dtype = float)
 model = Neural_network(lr=0.01, epochs=1000)
 model.fit(x, y)
-
-
+predictions = model.predict(x)
+print("Predictions:", predictions)
+plt.scatter(X, y, label="Real Data")
+plt.plot(X, predictions, label="Model", linestyle="--")
+plt.legend()
