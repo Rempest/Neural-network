d3643817cd57b961a7123991685b8c804faef1fd
Rempest <thehuman130410@gmail.com>
2026-04-15 19:50:40 +0500

    Rename Neural-network to Neural in train.py

--- train.py
+++ train.py
@@ -1,9 +1,9 @@
 import numpy as np
-from Neural-network import Neural-network
+from Neural import Neural
 import matplotlib.pyplot as plt
 x = np.array([1, 2, 3, 4, 5], dtype = float)
 y = np.array([2, 4, 6, 8, 10], dtype = float)
-model = Neural_network(lr=0.01, epochs=1000)
+model = Neural(lr=0.01, epochs=1000)
 model.fit(x, y)
 predictions = model.predict(x)
 print("Predictions:", predictions)
