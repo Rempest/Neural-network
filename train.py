85f924b7ce83493ec8657d6511689c250efed7af
Rempest <thehuman130410@gmail.com>
2026-04-15 19:35:05 +0500

    Fix model initialization and add fitting method

--- train.py
+++ train.py
@@ -3,5 +3,7 @@ from Neural-network import Neural-network
 import matplotlib.pyplot as plt
 x = np.array([1, 2, 3, 4, 5], dtype = float)
 y = np.array([2, 4, 6, 8, 10], dtype = float)
-model = (lr=0.01, epochs=1000)
+model = Neural_network(lr=0.01, epochs=1000)
+model.fit(x, y)
+
 
