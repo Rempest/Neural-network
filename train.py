1d15c2bca6e73a079d6a6293e51a4e5e5a292476
Rempest <thehuman130410@gmail.com>
2026-04-15 19:31:36 +0500

    Create train.py with initial setup
    Initialize training script with data and model parameters.

---
+++ train.py
@@ -0,0 +1,7 @@
+import numpy as np
+from Neural-network import Neural-network
+import matplotlib.pyplot as plt
+x = np.array([1, 2, 3, 4, 5], dtype = float)
+y = np.array([2, 4, 6, 8, 10], dtype = float)
+model = (lr=0.01, epochs=1000)
+
